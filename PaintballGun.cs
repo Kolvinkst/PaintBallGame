ceec0e5760dec54b22f2ea3c1178ff229508e6e3
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-14 00:01:22 +0700

    Добавьте файлы проекта.

---
+++ PaintballGun.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintBallGame
+{
+    internal class PaintballGun
+    {
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+        public int MagazineSize { get; private set; } = 16;
+
+        private int balls = 0;
+
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() { return BallsLoaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; } 
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else
+                BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintBallGame
+{
+    internal class Program
+    {
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            //Вывести сообщение, за которым следует [значение по умолчанию]:
+            //Прочитать строку из ввода и попытаться преобразовать её вызовом int.TryParse
+            //Если преобразование прошло успешно, вывести на консоль строку "   using value" + value.
+            //В противном случае вывести на консоль строку " using default value" + lastUsedValue
+            Console.WriteLine(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int numberOfBalls = ReadInt(20, "Number of balls");
+            int magazineSize = ReadInt(16, "Magazine size");
+
+            Console.Write($"Loaded [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+            PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Balls += gun.MagazineSize;
+                else if (key == 'q') return;
+            }
+        }
+    }
+}
